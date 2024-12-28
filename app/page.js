6d8d624cbb0eb50e62714d7130d3206c646cd994
Parthadeb Mondal <42791257+WebcoderPartha@users.noreply.github.com>
2024-12-28 14:51:08 +0600

    All blog api fetch done

--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
 import { getPosts } from "@/lib/getPosts";
 
-
 export default async function Home() {
-  const all_posts = await getPosts();
-  console.log(all_posts);
+  const posts = await getPosts();
+  console.log(posts)
   return (
-    <div className="border grid grid-cols-4 gap-2 px-4">
-      <div>sdfsdf</div>
-      <div>sdfsdf</div>
-      <div>sdfsdf</div>
+    <div className="grid grid-cols-4 gap-2 py-4">
+      {posts.map((post) => (
+        <div key={post.id} className="border px-2 py-2">
+          <h2 className="text-sm font-semibold">{post.title}</h2>
+         {post.tags.map((tag) => (<p key={tag}>{tag}</p>))}
+        </div>  
+
+      ))}
     </div>
   );
 }
